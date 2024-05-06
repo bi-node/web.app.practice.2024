e7db6ed36788fbad6ad7c99f6ad6140c49f44de6
Binod Rasaili <binod.toyam@gmail.com>
2024-05-06 17:07:01 -0500

    good

---
+++ JavaScript/Day01/Day2ECnClosures/scriptt.js
@@ -0,0 +1,7 @@
+// console.log(window);
+// console.log(document)
+// console.log(this);
+// console.log(globalThis===this);
+// console.log(this===window);
+
+        document.getElementById("myHeading").innerText = "Hello from JavaScript!";
---
+++ JavaScript/timeout.js
@@ -0,0 +1,11 @@
+const foo=function()
+{
+    console.log("hello");  
+};
+//console.log("hi");
+
+setTimeout(foo,10000);
+//console.log("blondie");
+
+setInterval(foo,2000);
+clearInterval;  
